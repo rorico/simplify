db82f7553546c7e9ca9d51899fc16bcae4b43def
EC2 Default User <ec2-user@ip-172-31-16-32.us-west-2.compute.internal>
2019-05-25 12:48:05 +0000

    more work

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var fs = require("fs")
 
 function simplify(code, fname, args) {
 	var funcs = {}
+	var vars = {}
 	var ast = acorn.parse(code)
 	var body = ast.body
 	for (var name in body) {
@@ -21,13 +22,13 @@ function simplify(code, fname, args) {
 		return 0
 	}
 	console.log(func)
-	var vars = {}
 	var params = func.params
 	for (var i = 0 ; i < params ; i++) {
 		vars[params[i].name] = args[i]
 	}
 	var changed = {}
 	funcs = {}
+	walk(body)
 	var body = func.body
 	function check(node) {
 		return walk(node, (n) => {
@@ -38,26 +39,35 @@ function simplify(code, fname, args) {
 	// function 
 
 	function walk(node) {
+		var ret = {
+			ret: null,
+			delete: false
+		}
+		var after
 		// todo hoisted and functions
 		switch (node.type) {
 			case "VariableDeclarator":
-				vars[node.id.name] = evalNode(node.init) && check(node.init)
+				//vars[node.id.name] = evalNode(node.init) && check(node.init)
+				after = (res) => {
+					vars[node.id.name] = res.init.ret
+				}
 				break
 			case "FunctionDeclaration":
 				funcs[node.id.name] = node
 				//console.log(node)
 				// todo seperate function into different closures
 				var params = node.params
-				console.log("asdnfkafsd", node)
-				for (var i = 0 ; i < params ; i++) {
-					vars[params[i].name] = null
+				//console.log("asdnfkafsd", node)
+				for (var i = 0 ; i < params.length ; i++) {
+					vars[params[i].name] = {}
 				}
-				break
-				return
+				//break
+				return ret
 			case "IfStatement":
 				// console.log("if test ", evalNode(node.test), node, node.test)
 				if (!evalNode(node.test)) {
-					return true
+					ret.delete = true
+					return ret
 				}
 				break
 			case "AssignmentExpression":
@@ -76,21 +86,54 @@ function simplify(code, fname, args) {
 			case "BinaryExpression":
 				break
 			case "BlockStatement":
-				console.log(node)
+				//console.log(node)
 				break
 			case "VariableDeclaration":
 				break
 			case "MemberExpression":
+				console.log("member", node)
+				after = (res) => {
+					if (node.computed) {
+						ret.ret = vars[res.object.ret][res.property.ret]
+					} else {
+						ret.ret = vars[res.object.ret][node.property.name]
+					}
+
+					console.log("mems", node, ret)
+				}
+				var test
 				break
 			case "ObjectExpression":
 				break
 			case "UnaryExpression":
+				//console.log("unary ", node)
+				after = (res) => {
+					console.log(res, res.test, "lebensn")
+					res = res.argument
+					if (node.operator === "!") {
+						ret.ret = !res.ret
+					} else if (node.operator === "+") {
+						ret.ret = +res.ret
+					} else if (node.operator === "-") {
+						ret.ret = -res.ret
+					} else if (node.operator === "typeof") {
+						ret.ret = typeof res.ret
+					} else {
+						console.log("unknown unary", node.operator)
+					}
+					return ret
+				}
 				break
 
 
 			case "Literal":
+				ret.ret = node.value
+				//console.log("lit ", node)
+				return ret
 				break
 			case "Identifier":
+				ret.ret = vars[node.name]
+				return ret
 				break
 			case "ReturnStatement":
 				break
@@ -99,26 +142,31 @@ function simplify(code, fname, args) {
 				break
 		}
 		// console.log("node", node)
+		var res = {}
 		for (var key in node) {
 			var val = node[key]
 			if (Array.isArray(val)) {
 				for (var i = 0 ; i < val.length ; i++) {
 					var c = val[i]
-					var res = walk(c)
-					if (res) {
+					// for now assume not used outside
+					res = walk(c)
+					if (res && res.delete) {
 						val.splice(i, 1)
 						i--
 					}
 				}
 			} else if (val && typeof val.type === "string") {
-				var res = walk(val)
-				if (res) node[key] = null
+				var r = res[key] = walk(val)
+				if (r && r.delete) node[key] = null
 			}
 		}
+		if (after) after(res)
+		return ret
 	}
 	function evalNode(node) {
 		if (!node) return undefined
 		// console.log("eval", node)
+		console.log(vars)
 		return evalContext("(" + astring.generate(node) + ")", vars)
 	}
 	function evalContext(str, context) {
